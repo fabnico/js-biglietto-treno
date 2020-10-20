40313a9f410c03ac7f6a992bad009dfc122627dc
Fabrizio <whoet.wo@gmail.com>
2020-10-20 15:47:34 +0200

    Esercizio completato

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var prezzobase = 0.21;
 var prezzotk = prezzobase * kmviaggio;
 
 if (eta < 18){
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.8;
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.8 + "€";
 }else if (eta > 65){
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.6;
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.6 + "€";
 }else{
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk;
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk + "€";
 }
