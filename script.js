59ab968d891b93f52e650b633cab3766b5b6b64e
Fabrizio <whoet.wo@gmail.com>
2020-10-20 15:45:44 +0200

    Esercizio completato

---
+++ script.js
@@ -0,0 +1,12 @@
+var kmviaggio = prompt("Quanti chilometri vuoi percorrere?");
+var eta = prompt("Qual è la tua età?");
+var prezzobase = 0.21;
+var prezzotk = prezzobase * kmviaggio;
+
+if (eta < 18){
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.8;
+}else if (eta > 65){
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.6;
+}else{
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk;
+}
