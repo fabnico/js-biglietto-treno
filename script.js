e3b2598e1a1ac1cffdda8b45f87121168478713d
Fabrizio <whoet.wo@gmail.com>
2020-10-20 17:14:17 +0200

    Fix calcolo percentuale e cifre decimali

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ var prezzobase = 0.21;
 var prezzotk = prezzobase * kmviaggio;
 
 if (eta < 18){
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.8 + "€";
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + (prezzotk - (prezzotk * 20 / 100)).toFixed(2) + "€";
 }else if (eta > 65){
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk * 0.6 + "€";
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + (prezzotk - (prezzotk * 40 / 100)).toFixed(2) + "€";
 }else{
-   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk + "€";
+   document.getElementById('buy').innerHTML = "Il tuo biglietto costa " + prezzotk.toFixed(2) + "€";
 }
